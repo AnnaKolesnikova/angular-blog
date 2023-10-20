74fa9bf2cd695426fe340f095af01b0cf9c2ab79
anna <ani.kolesnikova@gmail.com>
2023-10-20 17:05:58 +0200

    feat: add unsaved changes

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  Validators,
+} from '@angular/forms';
+import { User } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+  form: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
 
+  constructor() {}
+
+  ngOnInit() {
+    // console.log('init', this.form);
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
+  }
+
+  submit() {
+    // console.log('submit', this.form);
+    if (this.form?.invalid) {
+      return;
+    }
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password,
+    };
+  }
 }
