1856610c13da231fbe0660b254abe22702511fdb
anna <ani.kolesnikova@gmail.com>
2024-01-02 14:52:33 +0100

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Observable, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
 })
-export class AppComponent {}
+export class AppComponent {
+  posts$!: Observable<any[]>;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.posts$ = this.http
+      .get<any[]>(`https://jsonplaceholder.typicode.com/posts`)
+      .pipe(shareReplay());
+    const obs$ = fromTimestamp();
+    obs$.subscribe({ next: (value) => console.log(value) });
+    setTimeout(() => {
+      obs$.subscribe({ next: (value) => console.log(value) });
+    }, 2000);
+  }
+}
+
+const fromTimestamp = (): Observable<number> => {
+  const timestamp = Date.now();
+  return new Observable((subscriber) => {
+    subscriber.next(timestamp);
+  });
+};
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 import { routes } from './app/app-routing.module';
 import { RouterModule } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(RouterModule.forRoot(routes))],
+  providers: [
+    importProvidersFrom(RouterModule.forRoot(routes)),
+    provideHttpClient(),
+  ],
 }).catch((err) => console.error(err));
